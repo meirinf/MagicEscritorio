d9af97b5dbdb20f7741e5d931717245245b66492
Meirinf <meirin.f@gmail.com>
2016-11-21 20:28:01 +0100

    cambios fxml

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,6 +6,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.util.Callback;
 
 import java.util.ArrayList;
@@ -14,8 +16,10 @@ public class Controller {
 
     @FXML
     public ListView<Carta> lvLlistaCartes;
+    public ImageView imagen;
+    public TextField Texto;
 
-    public void initialize(){
+    public void initialize() {
 
         ApiCartas apicart = new ApiCartas();
         ArrayList<Carta> listacartas = apicart.getCardsTypes();
@@ -39,9 +43,8 @@ public class Controller {
         });
 
         // Afegir llista observable d'items
-        ObservableList<Carta> items = FXCollections.observableArrayList (
+        ObservableList<Carta> items = FXCollections.observableArrayList(
                 listacartas);
         lvLlistaCartes.setItems(items);
     }
-
 }
